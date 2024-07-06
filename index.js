963e2496be5b0652f5423d004abce994f16e727a
Adesewa Adeyemo <adeyemoadesewa54@gmail.com>
2024-07-06 15:16:20 +0100

    Assignment 2: Added javascript of a guess game to my html

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ function calcFizzBuzz(num) {
 }
 calcFizzBuzz(15);
 
-
 // Output:
 // FizBuzz;
 // 1;
@@ -31,3 +30,33 @@ calcFizzBuzz(15);
 // 13;
 // 14;
 // FizBuzz;
+
+window.addEventListener("load", () => {
+  let form = document.getElementById("form");
+  form.addEventListener("submit", SubmitGuess);
+});
+function SubmitGuess(event) {
+  event.preventDefault();
+  luckyNum = Math.floor(Math.random() * 11);
+
+  // let guess = new FormData(form);
+
+  // const data = Object.fromEntries(FormData)
+  // console.log(guess);
+
+  let guess = form.guess.value;
+
+  if (guess == luckyNum){
+    alert("You won");
+  }else{
+    alert("Oooops, You loss. The lucky number is " + luckyNum)
+  }
+}
+
+// const guesbtn = document.getElementById("guesbtn");
+// guesbtn.addEventListener((event) => {
+//   alert("seen");
+// });
+
+// const game = document.getElementsByClassName("game");
+// game.style.display = "none";
